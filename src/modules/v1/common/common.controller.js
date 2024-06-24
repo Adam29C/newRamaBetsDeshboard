785d6172cf9c5c485037fb8a647d9a8ae8a700db
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-06-24 17:40:01 +0530

    add the code

--- src/modules/v1/common/common.controller.js
+++ src/modules/v1/common/common.controller.js
@@ -7,40 +7,30 @@ import {
   BadRequestResponse,
 } from "./../../../helpers/http.js";
 import { JWT_EXPIRES_IN, JWT_SECRET } from "./../../../config/env.config.js";
-import { TokenData } from "../../../models/token.js";
-import admin from "../../../models/admin.js";
+import admin from "../../../models/admin.js"
 import mongoose from "mongoose";
 
 const generateAuthToken = async (req, res) => {
   try {
-    const { id, deviceId, role } = req.body;
+    const { id, deviceId } = req.body;
     let query;
     let details;
     let token;
+    let roles = "USER"; 
+
     if (id) {
-      if (role === "ADMIN") {
-        details = await findOne("admin", { _id: id });
-      } else {
-        details = await findOne("users", { _id: id });
-      }
+      details = await findOne("admin", { _id: id }) || await findOne("users", { _id: id });
       if (!details) {
         return BadRequestResponse(res, req.t("failure_message_2"));
       }
+      roles = details.role;
       query = { id: id };
     }
+
     if (deviceId) {
       query = { deviceId: deviceId };
     }
-    let rol = {};
-    if (role.toUpperCase() === "ADMIN") {
-      rol = { ADMIN: "ADMIN" };
-    } else if (role.toUpperCase() === "SUBADMIN") {
-      rol = { SUBADMIN: "SUBADMIN" };
-    } else {
-      rol = { USER: "USER" };
-    }
 
-    let roles = Object.keys(rol);
     token = jwt.sign(
       {
         info: {
@@ -55,6 +45,7 @@ const generateAuthToken = async (req, res) => {
         expiresIn: JWT_EXPIRES_IN,
       }
     );
+
     let data = await findOne("TokenData", query);
     if (data) {
       await update("TokenData", { _id: data._id }, { token: token });
@@ -63,12 +54,14 @@ const generateAuthToken = async (req, res) => {
         token: token,
         deviceId: deviceId ? deviceId : "",
       };
-      // Check if id is a valid ObjectId
-      if (mongoose.Types.ObjectId.isValid(id)) {
+
+      if (id && mongoose.Types.ObjectId.isValid(id)) {
         tokenData.userId = id;
       }
+
       await insertQuery("TokenData", tokenData);
     }
+
     return SuccessResponse(res, req.t("success_status"), { token });
   } catch (err) {
     return InternalServerErrorResponse(
--- src/modules/v1/common/common.schema.js
+++ src/modules/v1/common/common.schema.js
@@ -2,6 +2,5 @@ import joi from "joi";
 const generateAuthTokenSchema = joi.object({
     id: joi.string().optional(),
     deviceId: joi.string().optional(),
-    role: joi.string().valid("USER", "ADMIN", "SUBADMIN").required(),
 });
 export { generateAuthTokenSchema }
