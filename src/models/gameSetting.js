9d2dc419843f05d3eb100928b2c1ea91ce9c6ccb
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-07-05 14:01:28 +0530

    remove the unuse space

--- src/models/gameSetting.js
+++ src/models/gameSetting.js
@@ -1,42 +1,42 @@
 import mongoose from 'mongoose';
 const gamesSettingSchema = new mongoose.Schema({
-        gameType:{
-            type:String,
-            required:true
-        },
-        providerId: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true
-        },
-        gameDay:{
-            type: String,
-            required: true
-        },
-        OBT:{
-            type: String,
-            required: true
-        },
-        CBT:{
-            type: String,
-            required: true
-        },
-        OBRT:{
-            type: String,
-            required: true
-        },
-        CBRT:{
-            type: String,
-            required: true
-        },
-        isClosed:{
-            type: String,
-            required: true
-        },
+    gameType: {
+        type: String,
+        required: true
     },
-    {  
-        timestamps:true,
-        versionKey : false
+    providerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    gameDay: {
+        type: String,
+        required: true
+    },
+    OBT: {
+        type: String,
+        required: true
+    },
+    CBT: {
+        type: String,
+        required: true
+    },
+    OBRT: {
+        type: String,
+        required: true
+    },
+    CBRT: {
+        type: String,
+        required: true
+    },
+    isClosed: {
+        type: String,
+        required: true
+    },
+},
+    {
+        timestamps: true,
+        versionKey: false
     });
 
 const GameSetting = mongoose.model('GameSetting', gamesSettingSchema);
-export {GameSetting}
+export { GameSetting }
