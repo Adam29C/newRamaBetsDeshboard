f8b197e5d26d8df1ba3b23d39ae33762626a4405
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-07-30 16:15:11 +0530

    add the cdoe

--- src/modules/v1/admin/appSetting/appSetting.routers.js
+++ src/modules/v1/admin/appSetting/appSetting.routers.js
@@ -14,28 +14,10 @@ import { verifyToken } from "../../../../helpers/token.js";
 import { listVersionSetting, updateVersionSetting } from "./appSetting.controller.js";
 import getMulterStorage from "../../../../helpers/fileUpload.js";
 import { listVersionSettingSchema } from "./appSetting.schema.js";
+
 // Get current directory in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const adminDetailsRouters = express.Router();
-const systemInformition = getMulterStorage("uploads/systemInfo");
-
-const storage = multer.diskStorage({
-  destination: async function (req, file, cb) {
-    const tempDir = path.join(__dirname, '../../../../public/tempDirectory/');
-    await fs.mkdir(tempDir, { recursive: true }); // Ensure directory exists
-    cb(null, tempDir); // Temporary storage directory
-  },
-  filename: function (req, file, cb) {
-    const originalname = file.originalname;
-    const ext = path.extname(originalname);
-    const filename = `Ramabets-V${req.body.appVer}${ext}`;
-    cb(null, filename);
-  }
-});
-
-const upload = multer({ storage: storage });
 
 const appSettingRouters = express.Router();
 
@@ -43,21 +25,37 @@ const appSettingRouters = express.Router();
 appSettingRouters.use(express.json());
 appSettingRouters.use(express.urlencoded({ extended: true }));
 
+const storage = multer.diskStorage({
+    destination: async function (req, file, cb) {
+        const tempDir = path.join(__dirname, '../../../../public/tempDirectory/');
+        await fs.mkdir(tempDir, { recursive: true }); // Ensure directory exists
+        cb(null, tempDir); // Temporary storage directory
+    },
+    filename: function (req, file, cb) {
+        const originalname = file.originalname;
+        const ext = path.extname(originalname);
+        console.log(req.body.appVer, "hhhhhhhhhh");
+        const filename = `Ramabets-V${req.body.appVer}${ext}`;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 appSettingRouters.put(
-  '/updateVersionSetting',
-  upload.single('apk'),
-  verifyToken,
-  verifyRoles(roleList.ADMIN),
-  // validator(updateVersionSettingSchema, ValidationSource.BODY),
-  updateVersionSetting
+    '/updateVersionSetting',
+    verifyToken,
+    verifyRoles(roleList.ADMIN),
+    upload.single('apk'),
+    updateVersionSetting
 );
 
 appSettingRouters.get(
-  '/listVersionSetting',
-  verifyToken,
-  verifyRoles(roleList.ADMIN),
-  validator(listVersionSettingSchema, ValidationSource.QUERY),
-  listVersionSetting
+    '/listVersionSetting',
+    verifyToken,
+    verifyRoles(roleList.ADMIN),
+    validator(listVersionSettingSchema, ValidationSource.QUERY),
+    listVersionSetting
 );
 
 export { appSettingRouters };
