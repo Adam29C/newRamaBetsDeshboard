523c7498b29643bc5ac0b6e34b638a714f48c85a
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-10-07 11:09:37 +0530

    add the code

--- src/models/gameBids.js
+++ src/models/gameBids.js
@@ -1,81 +1,86 @@
-import mongoose from 'mongoose';
-const game_bidsSchema = new mongoose.Schema({
-        userId: {
-            type: String,
-            required: true
-        },
-        providerId: {
-            type: String,
-            required: true
-        },
-        gameTypeId: {
-            type: String,
-            required: false
-        },
-        providerName: {
-            type: String,
-            required: true
-        }, 
-        gameTypeName:{
-            type: String,
-            required: false
-        },
-        gameTypePrice:{
-            type: Number,
-            required: true
-        },
-        userName: {
-            type: String,
-            required: true
-        },
-        mobileNumber: {
-            type: String,
-            required: true
-        }, 
-        bidDigit:{
-            type: String,
-            required: true
-        },
-        biddingPoints:{
-            type: Number,
-            required: true
-        },
-        winStatus:{
-            type: Number,
-            required: true
-        },
-        gameWinPoints:{
-            type: Number,
-            required: true
-        },
-        gameDate:{
-            type: String,
-            required: true
-        },
-        dateStamp:{
-            type: Number,
-            required: false
-        },
-        gameSession:{
-            type: String,
-            required: true
-        },
-        createdAt:{
-            type: String,
-            required: true
-        },
-        updatedAt:{
-            type: String,
-            required: true
-        }
+import { required } from "joi";
+import mongoose from "mongoose";
+const game_bidsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey : false,
-        timestamps: {
-            createdAt: 'createTime',
-            updatedAt: 'updatedTime'
-        }
-    });
+    cardId: { type: String, required: true },
+    cardName:{type: String, required: true},
+    providerId: {
+      type: String,
+      required: true,
+    },
+    gameTypeId: {
+      type: String,
+      required: false,
+    },
+    providerName: {
+      type: String,
+      required: true,
+    },
+    gameTypeName: {
+      type: String,
+      required: false,
+    },
+    gameTypePrice: {
+      type: Number,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+    },
+    bidDigit: {
+      type: String,
+      required: true,
+    },
+    biddingPoints: {
+      type: Number,
+      required: true,
+    },
+    winStatus: {
+      type: Number,
+      required: true,
+    },
+    gameWinPoints: {
+      type: Number,
+      required: true,
+    },
+    gameDate: {
+      type: String,
+      required: true,
+    },
+    dateStamp: {
+      type: Number,
+      required: false,
+    },
+    gameSession: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: String,
+      required: true,
+    },
+    updatedAt: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "createTime",
+      updatedAt: "updatedTime",
+    },
+  }
+);
 
-const game_bids = mongoose.model('game_bids', game_bidsSchema);
-export{game_bids}
+const game_bids = mongoose.model("game_bids", game_bidsSchema);
+export { game_bids };
